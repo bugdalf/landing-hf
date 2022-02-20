15cff66e30c2c72ae1808b5bf7a15869366b0d04
Vander Idme <bugdalf@gmail.com>
2022-02-20 00:24:07 -0500

    responsive HERO

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const modulos = document.getElementsByClassName("programas--card__modulo")
+const carouselContainer = document.getElementsByClassName("hero--carousel__container")
 
 const expandirModulo = function() {
   let estado = this.getAttribute("data-atributo")
@@ -14,3 +15,14 @@ const expandirModulo = function() {
 for(let i = 0; i < modulos.length; i++){
   modulos[i].addEventListener('click', expandirModulo)
 }
+
+carouselContainerWidth = carouselContainer[0].offsetWidth
+carouselContainer[0].style.height = carouselContainerWidth + 'px'
+
+window.addEventListener('resize', () => {
+  carouselContainerWidth = carouselContainer[0].offsetWidth
+  carouselContainer[0].style.height = carouselContainerWidth + 'px'
+  console.log(typeof carouselContainerWidth)
+})
+
+
