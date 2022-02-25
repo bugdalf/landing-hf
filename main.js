ea8c42fc6177a604de57ef7008da3e1f4f96325b
Vander Idme <bugdalf@gmail.com>
2022-02-24 23:33:52 -0500

    refactor de la seccion programas de estudio

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-const modulos = document.getElementsByClassName("programas--card__modulo")
 const carouselContainer = document.getElementsByClassName("hero--carousel__container")
-
-const expandirModulo = function() {
-  let estado = this.getAttribute("data-atributo")
-  if(estado === 'false'){
-    this.classList.add("expandir-modulo")
-    this.setAttribute("data-atributo", 'true')
-  } else if(estado){
-    this.classList.remove("expandir-modulo")
-    this.setAttribute("data-atributo", 'false')
-  }
-};
-
-for(let i = 0; i < modulos.length; i++){
-  modulos[i].addEventListener('click', expandirModulo)
-}
 
 carouselContainerWidth = carouselContainer[0].offsetWidth
 carouselContainer[0].style.height = carouselContainerWidth + 'px'
@@ -24,4 +8,24 @@ window.addEventListener('resize', () => {
   carouselContainer[0].style.height = carouselContainerWidth + 'px'
 })
 
+const listaModulo = document.getElementsByClassName("modulo-lista")
+const botones = document.getElementsByClassName("modulo-botones")
 
+function expandirModulo () {
+  let indice = this.getAttribute("data-modulo")
+  let estadoLista = listaModulo[indice].getAttribute("data-estado")
+  if(estadoLista === 'false') {
+    listaModulo[indice].style.display = "block"
+    listaModulo[indice].setAttribute("data-estado",'true') 
+  } else if(estadoLista === 'true') {
+    listaModulo[indice].style.display = "none"
+    listaModulo[indice].setAttribute("data-estado",'false') 
+  }
+
+}
+
+for(let i = 0; i<botones.length; i++) {
+  botones[i].addEventListener('click', expandirModulo)
+  botones[i].setAttribute("data-modulo", i)
+  listaModulo[i].setAttribute("data-lista", i)
+}
